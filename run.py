9d309a0efd6dcbba13a035833ab0d97c32c02b9b
makseli <makseli>
2019-12-12 01:17:58 +0300

    lets some changes

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 # -*- coding:utf-8 -*-
 
-
+import os
 from dotenv import load_dotenv
 from mailchimp3 import MailChimp
 
 load_dotenv()
 
-client = MailChimp(mc_api='', mc_user='verilojistik')
+# client = MailChimp(mc_api=os.getenv('MC_API_KEY'), mc_user=os.getenv('MC_USER_NAME'))
 
 # aa = client.lists.all(get_all=True, fields="lists.name,lists.id")
-aa = client.lists.members.all('58c9187424', True, fields="members.email_address,members.id")
+# aa = client.lists.members.all('58c9187424', True, fields="members.email_address,members.id")
+aa = 'https://us4.api.mailchimp.com/3.0/lists/58c9187424/members' # YA da buraya curl&requests ile get gönderebilirsin !
 print(aa)
